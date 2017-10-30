14d9195ef2c1ecbb0e3ee12d008e4c06f9532a43
Balázs Gyöngyösi <balux33@gmail.com>
2017-10-30 13:26:37 +0100

    Version 1.1

---
+++ bm1d_build/CMakeFiles/BM1D.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hallgato3/hall3git/BrownM3/BM1D.cc" "/home/hallgato3/hall3git/BrownM3/bm1d_build/CMakeFiles/BM1D.dir/BM1D.cc.o"
+  "/home/hallgato3/hall3git/BrownM3/src/BM1DProcess.cc" "/home/hallgato3/hall3git/BrownM3/bm1d_build/CMakeFiles/BM1D.dir/src/BM1DProcess.cc.o"
+  "/home/hallgato3/hall3git/BrownM3/src/Plotter.cc" "/home/hallgato3/hall3git/BrownM3/bm1d_build/CMakeFiles/BM1D.dir/src/Plotter.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/root/include"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/BM1DProcess.cc
+++ src/BM1DProcess.cc
@@ -1,14 +1,22 @@
 #include "BM1DProcess.hh"
 
-BM1DProcess::BM1DProcess(Int_t nP) : 
-  nSteps (nP), p0(0.5), p1(0.3)
+BM1DProcess::BM1DProcess(Int_t nP) : nSteps (nP), p0(0.5), p1(0.3), gaussMean(0), gaussSigma(1)
 {
   randomGenerator = new TRandom();
+  randomGeneratorGauss = new TRandom();
+}
+
+BM1DProcess::BM1DProcess(Int_t nP, double mean, double sigma) : nSteps (nP), p0(0.5), p1(0.3), gaussMean(mean), gaussSigma(sigma)
+{
+  randomGeneratorGauss = new TRandom();
+  randomGenerator = new TRandom();
+  std::cout << randomGeneratorGauss -> GetSeed() <<std::endl;
+  randomGenerator -> SetSeed(0);
+  randomGeneratorGauss -> SetSeed(0);
 }
 
 BM1DProcess::~BM1DProcess()
 {
-  ;
 }
 
 void BM1DProcess::Init(){
@@ -17,24 +25,34 @@ void BM1DProcess::Init(){
 }
 
 void BM1DProcess::Run(){
+	
+	double tmpM = gaussMean, tmpS = gaussSigma;
+	
   for (Int_t i = 1;i < nSteps;i++)
     {
+		if((x[i-1] > 20) && (x[i-1] < 30) )
+		{
+			gaussSigma /= 5 ;
+		}
+		else
+		{
+			gaussSigma = tmpS;
+		}
+		
       rand1 = randomGenerator->Uniform();
 	if(rand1 < p0) {  //step in time, but no step in x
 	  t.push_back(t[i-1] + 1);
 	  x.push_back(x[i-1]);
-	}
+		}
+		
 	else {//step left or right
-	  if(rand1 < p0 + p1)
-	    {
+	 double randGauss = randomGenerator->Gaus(gaussMean, gaussSigma);
+	 std::cout <<"i:" << i << "rndgaus:" << randGauss << std::endl;
+	  
 	      t.push_back(t[i-1]+1);
-	      x.push_back(x[i-1]+1); //one step right
-	    }
-	  else
-	    {
-	      t.push_back(t[i-1]+1);
-	      x.push_back(x[i-1]-1); //one step left
-	    }
-	}
+	      x.push_back(x[i-1]+randGauss); 
+	   
+	     
+		}
     }
 }
